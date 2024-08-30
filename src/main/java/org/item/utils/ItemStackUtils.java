d88393585792801b3d0ec5f88a6a8253496d7e74
BuildTools <seunghun9080@gmail.com>
2024-08-30 21:18:12 +0900

    Update For Paper 1.21.1

--- src/main/java/org/item/utils/ItemStackUtils.java
+++ src/main/java/org/item/utils/ItemStackUtils.java
@@ -1,45 +1,23 @@
 package org.item.utils;
 
-import io.papermc.paper.inventory.ItemRarity;
-import net.kyori.adventure.key.Key;
-import net.kyori.adventure.nbt.api.BinaryTagHolder;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.ItemRarity;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.function.UnaryOperator;
 
 public class ItemStackUtils {
 	public static Component toComponentwithHoverEvent(ItemStack item) {
-		Component itemcomponent;
-		if(item.getType().getItemRarity().equals(ItemRarity.COMMON) || item.getType().getItemRarity().equals(ItemRarity.UNCOMMON)) {
-			if (item.hasItemMeta()) {
-				if(item.getItemMeta().hasEnchants()) {
-					if (item.getItemMeta().hasDisplayName()) {
-						itemcomponent = Component.text("[").color(NamedTextColor.AQUA).append(item.getItemMeta().displayName().color(NamedTextColor.AQUA)).hoverEvent(HoverEvent.showItem(Key.key(Key.MINECRAFT_NAMESPACE, item.getType().toString().toLowerCase()), 1, BinaryTagHolder.binaryTagHolder(item.getItemMeta().getAsString()))).append(Component.text("]")).color(NamedTextColor.AQUA);
-					} else {
-						itemcomponent = Component.text("[").color(NamedTextColor.AQUA).append(Component.translatable(item.getType().translationKey())).color(NamedTextColor.AQUA).hoverEvent(HoverEvent.showItem(Key.key(Key.MINECRAFT_NAMESPACE, item.getType().toString().toLowerCase()), 1, BinaryTagHolder.binaryTagHolder(item.getItemMeta().getAsString()))).append(Component.text("]")).color(NamedTextColor.AQUA);
-					}
-				} else {
-					if (item.getItemMeta().hasDisplayName()) {
-						itemcomponent = Component.text("[").color(item.getType().getItemRarity().getColor()).append(item.getItemMeta().displayName().color(item.getType().getItemRarity().getColor())).hoverEvent(HoverEvent.showItem(Key.key(Key.MINECRAFT_NAMESPACE, item.getType().toString().toLowerCase()), 1, BinaryTagHolder.binaryTagHolder(item.getItemMeta().getAsString()))).append(Component.text("]")).color(item.getType().getItemRarity().getColor());
-					} else {
-						itemcomponent = Component.text("[").color(item.getType().getItemRarity().getColor()).append(Component.translatable(item.getType().translationKey())).color(item.getType().getItemRarity().getColor()).hoverEvent(HoverEvent.showItem(Key.key(Key.MINECRAFT_NAMESPACE, item.getType().toString().toLowerCase()), 1, BinaryTagHolder.binaryTagHolder(item.getItemMeta().getAsString()))).append(Component.text("]")).color(item.getType().getItemRarity().getColor());
-					}
-				}
-			} else {
-				itemcomponent = Component.text("[").color(item.getType().getItemRarity().getColor()).append(Component.translatable(item.getType().translationKey())).color(item.getType().getItemRarity().getColor()).hoverEvent(HoverEvent.showItem(Key.key(Key.MINECRAFT_NAMESPACE, item.getType().toString().toLowerCase()), 1)).append(Component.text("]")).color(item.getType().getItemRarity().getColor());
-			}
-		} else {
-			if (item.hasItemMeta()) {
-				if (item.getItemMeta().hasDisplayName()) {
-					itemcomponent = Component.text("[").color(item.getType().getItemRarity().getColor()).append(item.getItemMeta().displayName().color(item.getType().getItemRarity().getColor())).hoverEvent(HoverEvent.showItem(Key.key(Key.MINECRAFT_NAMESPACE, item.getType().toString().toLowerCase()), 1, BinaryTagHolder.binaryTagHolder(item.getItemMeta().getAsString()))).append(Component.text("]")).color(item.getType().getItemRarity().getColor());
-				} else {
-					itemcomponent = Component.text("[").color(item.getType().getItemRarity().getColor()).append(Component.translatable(item.getType().translationKey())).color(item.getType().getItemRarity().getColor()).hoverEvent(HoverEvent.showItem(Key.key(Key.MINECRAFT_NAMESPACE, item.getType().toString().toLowerCase()), 1, BinaryTagHolder.binaryTagHolder(item.getItemMeta().getAsString()))).append(Component.text("]")).color(item.getType().getItemRarity().getColor());
-				}
-			} else {
-				itemcomponent = Component.text("[").color(item.getType().getItemRarity().getColor()).append(Component.translatable(item.getType().translationKey())).color(item.getType().getItemRarity().getColor()).hoverEvent(HoverEvent.showItem(Key.key(Key.MINECRAFT_NAMESPACE, item.getType().toString().toLowerCase()), 1)).append(Component.text("]")).color(item.getType().getItemRarity().getColor());
-			}
-		}
-		return itemcomponent;
+		TextColor color;
+		if (item.hasItemMeta() && item.getItemMeta().hasRarity())
+            if (item.getItemMeta().getRarity().equals(ItemRarity.COMMON) || item.getItemMeta().getRarity().equals(ItemRarity.UNCOMMON))
+                color = item.getItemMeta().hasEnchants() ? NamedTextColor.AQUA : item.getItemMeta().getRarity().color();
+            else color = item.getItemMeta().getRarity().color();
+        else color = NamedTextColor.WHITE;
+		Component itemName = item.hasItemMeta() && item.getItemMeta().hasDisplayName() ? item.getItemMeta().displayName() : Component.translatable(item.getType().translationKey()).color(color);
+        return Component.empty().append(Component.text("[").color(color)).append(itemName.hoverEvent(Bukkit.getItemFactory().asHoverEvent(item, UnaryOperator.identity()))).append(Component.text("]").color(color));
 	}
 }
